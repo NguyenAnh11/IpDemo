2747fd65e12b25b6215dab38e575735bc73cd916
NguyenAnh11 <60679961+NguyenAnh11@users.noreply.github.com>
2023-03-14 16:54:08 +0700

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.get('/', (req, res) => {
 
     const cmd = `curl -s http://checkip.amazonaws.com || printf "0.0.0.0"`;
     const pubIp = execSync(cmd).toString().trim();
-    let { ip } = req
+    let ip = req.ip
     if (ip.substring(0, 7) === '::ffff:') {
         ip = ip.substring(7)
     }
