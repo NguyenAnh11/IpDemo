d9f400e05d14df2b256a2aed206423b70ae5c30e
Viet Anh <anh@tuivietnam.com>
2023-03-14 14:52:54 +0700

    done

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const requestIp = require('request-ip')
+
+const app = express()
+
+app.use(requestIp.mw())
+
+app.get('/', (req, res) => {
+    const { execSync } = require("child_process");
+
+    const cmd = `curl -s http://checkip.amazonaws.com || printf "0.0.0.0"`;
+    const pubIp = execSync(cmd).toString().trim();
+    res.send(pubIp)
+})
+
+app.listen(3000)
